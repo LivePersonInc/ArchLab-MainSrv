7a7927ae9c155904b79a85aa4920706041fa5e2d
Efim Dimenstein <efim@efimd.com>
2014-10-11 13:46:30 +0200

    added logger and elastic proxy

--- routes/main.js
+++ routes/main.js
@@ -5,8 +5,11 @@
 var express = require('express');
 var router = express.Router();
 
+var log = require('../lib/logger');
+
 /* GET home page. */
 router.get('/', function(req, res) {
+    log.info('Got request for the homepage');
     res.render('index', { title: 'Architecture Labs' });
 });
 
