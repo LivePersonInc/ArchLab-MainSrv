66cb74b252f45048bcd608dfcb3609030ffc9c9f
Efim Dimenstein <efim@efimd.com>
2014-10-11 19:43:44 +0200

    added a link to LogStash fixed handling of all http verbs

--- routes/opentsdb.js
+++ routes/opentsdb.js
@@ -11,40 +11,20 @@ var log = require('../lib/logger');
 var request = require('request');
 
 
-router.get('/', function(req, res) {
+router.all('/', function(req, res) {
+
     var url = req.originalUrl.replace('/opentsdb', '');
 
-    log.info('Serving Opentsdb proxy to url='+ url);
+    log.info('Serving Opentsdb proxy to method:'+req.method + ' url='+ url);
 
     var options = {
         url : 'http://54.208.5.227:4242' + url,
-        method : 'get',
+        method : req.method,
         headers : req.headers
     };
-
-    request(options, function(error, response, body) {
-        log.info('GOT response from Opentsdb error='+error);
-        for (var name in response.headers) {
-            res.setHeader(name, response.headers[name]);
-        }
-        res.send(body);
-    });
-});
-
-
-router.post('/', function(req, res) {
-
-    var url = req.originalUrl.replace('/opentsdb', '');
-
-    log.info('Serving Opentsdb proxy to url='+ url);
-    console.log(req.body);
-
-    var options = {
-        url : 'http://54.208.5.227:4242' + url,
-        method : 'post',
-        headers : req.headers,
-        body : JSON.stringify(req.body)
-    };
+    if (req.method=='POST' || req.method=='PUT') {
+        options.body = JSON.stringify(req.body);
+    }
 
     request(options, function(error, response, body) {
         log.info('GOT response from Opentsdb error='+error);
