ac6612f5b6ac3f792cf6977426fbe181d9ce193a
Efim Dimenstein <efim@efimd.com>
2014-10-13 19:25:19 +0200

    added landing page

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var app = express();
 
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('view engine', 'jade');
 
 
 app.use(bodyParser.json());
@@ -26,11 +26,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/apps/grafana', express.static(path.join(__dirname, 'apps/grafana')));
 app.use('/apps/kibana', express.static(path.join(__dirname, 'apps/kibana')));
 
-app.use('/', mainRoutes);
+//app.use('/', mainRoutes);
 app.use('/elastic/*', elasticRoutes);
 app.use('/opentsdb/*', opentsdbRoutes);
 
 app.set('port', 8888);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+//    var err = new Error('Not Found');
+//    err.status = 404;
+    res.writeHead(302, {
+        'Location': '/404.html'
+        //add other headers here...
+    });
+    res.end();
+});
 
 var server = app.listen(app.get('port'), function() {
     log.info('Express server listening on port ' + server.address().port);
